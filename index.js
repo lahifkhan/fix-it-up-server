a8b4bd81b5fe12f1cbc9234e97d9049f7841f82e
lahifkhan <lahifkhan52@gmail.com>
2025-11-12 21:11:40 +0600

    some setup done for vercel deploy

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("fixitDb");
     const issuesCollection = db.collection("issues");
@@ -131,7 +131,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
